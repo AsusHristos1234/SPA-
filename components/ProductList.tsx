1e0a3ca712b85371389a82735f2fd30e77344f1c
AsusHristos1234 <rq590590590@gmail.com>
2025-11-08 19:54:09 +0300

    карточки товаров в сетке

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -16,7 +16,7 @@ export default function ProductList({ products }: ProductListProps) {
   }
 
   return (
-    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+    <div className="product-grid">
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
